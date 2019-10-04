8d946b7805573f3a85786f89af1ffbf14f5bfe39
saurabh8522 <saurabhsinghal.171co240@nitk.edu.in>
2019-10-05 00:08:37 +0530

    second commit

--- first_project/first_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from first_app.models import WebPage,AccessRecord,Topic
-# Register your models here.
-admin.site.register(WebPage)
-admin.site.register(AccessRecord)
-admin.site.register(Topic)

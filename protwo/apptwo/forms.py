d0dd545e7dd8c0a4c42ded3f3f471fc1b1fe193f
saurabh8522 <saurabhsinghal.171co240@nitk.edu.in>
2019-10-04 20:36:28 +0530

    try new

--- protwo/apptwo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from apptwo.models import User
-
-class MyForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = '__all__'
--- protwo/apptwo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include
-from django.urls import path
-from apptwo import views
-
-urlpatterns=[
-	path('',views.index,name='index'),
-    path('help',views.help,name='help'),
-	path('user',views.user,name='user')
-]

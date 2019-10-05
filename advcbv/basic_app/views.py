43ad89c1ad9d1a7922013082c229ad5dba7a016c
saurabh8522 <saurabhsinghal.171co240@nitk.edu.in>
2019-10-05 16:31:03 +0530

    imp project

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View,TemplateView
 from django.http import HttpResponse
 # Create your views here.
 # def index(request):
 #     return render(request,'index.html')
+class IndexView(TemplateView):
+    template_name='index.html'
+    
+
+
+
+
+
 class CBView(View):
     def get(self,request):
         return HttpResponse('Class Based Views are Cool!')

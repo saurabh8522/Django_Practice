b16d97c74402d96754b33dc24fc2925b84940f30
saurabh8522 <saurabhsinghal.171co240@nitk.edu.in>
2019-10-07 13:05:18 +0530

    till cbvs

--- advcbv/advcbv/urls.py
+++ advcbv/advcbv/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from basic_app import views
 urlpatterns = [
-    path('',views.TemplateView.as_view(),name='index'),
+    path('',views.IndexView.as_view(),name='index'),
     path('admin/', admin.site.urls),
 ]
--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import (View,TemplateView,
+                                ListView,DetailView,
+                                CreateView,DeleteView,
+                                UpdateView)
 from django.http import HttpResponse
+from . import models
 # Create your views here.
 # def index(request):
 #     return render(request,'index.html')
 class IndexView(TemplateView):
     template_name='index.html'
-    
+
+    def get_context_data(self,**kwargs):
+        context  = super().get_context_data(**kwargs)
+        context['injectme'] = "Basic Injection!"
+        return context
 
 
 
